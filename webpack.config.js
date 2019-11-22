b3747f585270c9028afad05dd43891a2d83fd21d
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-11-22 11:31:04 +0300

    Fixes (webpack dev server url and ignore file)

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: "source-map",
   devServer: {
     contentBase: path.join( __dirname, "public"),
-    publicPath: "http: //localhost:7777/",
+    publicPath: "/",
     compress: true,
     watchContentBase: true
   }
