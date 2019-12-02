6e064a13307ded5f189e6fbd232876aebb6552d0
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-12-02 13:30:41 +0300

    Shows full route at the beginning of the page

--- src/components/route.js
+++ src/components/route.js
@@ -1,10 +1,15 @@
-export const createRouteTemplate = (routeInfo) => {
-  const startCity = routeInfo[0].city;
-  const endCity = routeInfo[routeInfo.length - 1].city;
+const getRouteHtml = (routeInfo) => {
+  if (routeInfo.length <= 3) {
+    return `${routeInfo[0].city} &mdash; ${routeInfo[1].city} &mdash; ${routeInfo[2].city}`;
+  }
 
+  return `${routeInfo[0].city} &mdash; ... &mdash; ${routeInfo[routeInfo.length - 1].city}`;
+};
+
+export const createRouteTemplate = (routeInfo) => {
   return (`
     <div class="trip-info__main">
-      <h1 class="trip-info__title">${startCity} &mdash; ... &mdash; ${endCity}</h1>
+      <h1 class="trip-info__title">${getRouteHtml(routeInfo)}</h1>
 
       <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
     </div>
