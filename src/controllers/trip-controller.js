6472ceb60e61263ddbacf718713d2b5f9d7aa0e5
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-12-10 21:08:44 +0300

    Adds sorting

--- src/controllers/trip-controller.js
+++ src/controllers/trip-controller.js
@@ -1,5 +1,5 @@
 import {render, RenderPosition, replace} from "../utils/render";
-import Sort from "../components/sort";
+import Sort, {SortType} from "../components/sort";
 import {sortOptions} from "../mock/sort";
 import Event from "../components/event";
 import Task from "../components/task";
@@ -12,20 +12,48 @@ export default class TripController {
   constructor(container, header) {
     this._container = container;
     this._header = header;
+    this._sortComponent = new Sort(sortOptions);
   }
 
   render(cards) {
     const tripRoute = this._header.querySelector(`.trip-main__trip-info`);
     const totalPrice = this._header.querySelector(`.trip-info__cost-value`);
 
-    render(this._container, new Sort(sortOptions));
-    this.setEventSortActive();
+    render(this._container, this._sortComponent);
+    this._sortComponent.setEventSortActive();
 
     render(this._container, new Event());
     render(this._container, new Task());
 
     const eventsList = this._container.querySelector(`.trip-events__list`);
 
+    this.renderCards(eventsList, cards);
+
+    render(tripRoute, new Route(cards), RenderPosition.AFTERBEGIN);
+    totalPrice.textContent = this.getTotalSum(cards);
+
+    this._sortComponent.setSortTypeChangeHandler((sortType) => {
+      let sortedTasks = [];
+
+      switch (sortType) {
+        case SortType.TIME:
+          sortedTasks = cards.slice().sort((a, b) => b.start - a.start);
+          break;
+        case SortType.PRICE:
+          sortedTasks = cards.slice().sort((a, b) => b.price - a.price);
+          break;
+        default:
+          sortedTasks = cards.slice(0);
+          break;
+      }
+
+      eventsList.innerHTML = ``;
+
+      this.renderCards(eventsList, sortedTasks);
+    });
+  }
+
+  renderCards(container, cards) {
     cards.forEach((card) => {
       const cardComponent = new Card(card);
       const editButton = cardComponent.getElement().querySelector(`.event__rollup-btn`);
@@ -61,28 +89,8 @@ export default class TripController {
 
       editButton.addEventListener(`click`, onEditButtonClick);
 
-      render(eventsList, cardComponent);
+      render(container, cardComponent);
     });
-    render(tripRoute, new Route(cards), RenderPosition.AFTERBEGIN);
-    totalPrice.textContent = this.getTotalSum(cards);
-  }
-
-  setSortItemChecked(sortItem) {
-    const sortItems = this._container.querySelectorAll(`.trip-sort__input`);
-
-    for (const sortItem of sortItems) {
-      if (sortItem.hasAttribute(`checked`)) {
-        sortItem.removeAttribute(`checked`);
-        break;
-      }
-    }
-
-    sortItem.setAttribute(`checked`, `checked`);
-  }
-
-  setEventSortActive() {
-    const sortItems = this._container.querySelectorAll(`.trip-sort__input`);
-    this.setSortItemChecked(sortItems[0]);
   }
 
   getTotalSum(tripPoints) {
